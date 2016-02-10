fcdf7ce1134adc0ca459758781322382de63fcb0
Nick Pantic <nmpantic@cpp.edu>
2016-02-10 14:54:24 -0800

    first commit

---
+++ String.cpp
@@ -0,0 +1,7 @@
+#include "String.h"
+
+// this header has some useful functions
+// for working with C-strings
+#include <cstring>
+
+// Put your function implementations here
---
+++ String.h
@@ -0,0 +1,19 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <iostream>
+#include <string>
+
+class StringTester;
+
+class String
+{
+    friend class StringTester;
+
+private:
+    // declare private members here
+public:
+    // declare public members here
+};
+
+#endif
